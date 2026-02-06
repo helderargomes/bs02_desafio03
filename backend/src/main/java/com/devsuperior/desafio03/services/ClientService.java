657fe47606a12eab3d5ebe5d5bbd91148330fb38
helderargomes <helderargomes@gmail.com>
2026-02-06 09:55:01 -0300

    Exception handling - Client delete

--- backend/src/main/java/com/devsuperior/desafio03/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/desafio03/services/ClientService.java
@@ -55,6 +55,9 @@ public class ClientService {
 	
 	@Transactional
 	public void delete(Long id) {
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
 		repository.deleteById(id);
 	}
 	
